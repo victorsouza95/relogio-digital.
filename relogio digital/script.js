8cd1ffc783a9133bda36f073e060c7eb99d780ff
victorsouza95 <156461797+victorsouza95@users.noreply.github.com>
2025-03-30 13:04:43 -0300

    Update script.js

--- relogio digital/script.js
+++ relogio digital/script.js
@@ -1,16 +1,16 @@
 // script.js
 function updateClock() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');  // Formatar hora
-    const minutes = String(now.getMinutes()).padStart(2, '0');  // Formatar minutos
-    const seconds = String(now.getSeconds()).padStart(2, '0');  // Formatar segundos
+    const hours = String(now.getHours()).padStart(2, '0');  
+    const minutes = String(now.getMinutes()).padStart(2, '0');  
+    const seconds = String(now.getSeconds()).padStart(2, '0');  
 
     const timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById('clock').textContent = timeString;
 }
 
-// Atualizar o relógio a cada segundo
+
 setInterval(updateClock, 1000);
 
-// Chama a função imediatamente para exibir a hora ao carregar a página
+
 updateClock();
